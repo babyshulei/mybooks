c3399f322d08065d1b0e6508cae1514f20d96e8f
Shulei <wang_shulei@foxmail.com>
2019-07-05 02:44:12 +0000

    feat: 添加插件，添加build.sh

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#build gitbooks
+
+target=${1%/}
+
+case "$target" in
+    es6)
+        gitbook build ./es6/ /home/$USER/gitbooks/es6/
+    ;;
+    http)
+        gitbook build ./http/ /home/$USER/gitbooks/http/
+    ;;
+    computer)
+	gitbook build ./computer/ /home/$USER/gitbooks/computer/
+    ;;
+    reading)
+	gitbook build ./reading/ /home/$USER/gitbooks/reading/
+    ;;
+    vue)
+	gitbook build ./vue/ /home/$USER/gitbooks/vue/
+    ;;
+    all)
+	gitbook build ./es6/ /home/$USER/gitbooks/es6/
+	gitbook build ./http/ /home/$USER/gitbooks/http/
+	gitbook build ./computer/ /home/$USER/gitbooks/computer/
+	gitbook build ./reading/ /home/$USER/gitbooks/reading/
+	gitbook build ./vue/ /home/$USER/gitbooks/vue/
+    ;;
+esac
